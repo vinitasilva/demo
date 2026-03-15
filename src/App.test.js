c32cda5b6aa5014a4ba2a695e43e2161afb986d9
vinitasilva <88274127+vinitasilva@users.noreply.github.com>
2026-03-15 20:16:25 +0000

    Update App.test.js
    update test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders Create User heading', () => {
   render(<App />);
-  const headingElement = screen.getByText(/create user/i);
+  const headingElement = screen.getByRole('heading', { name: /create user/i });
   expect(headingElement).toBeInTheDocument();
+});
+
+test('renders Create User button', () => {
+  render(<App />);
+  const buttonElement = screen.getByRole('button', { name: /create user/i });
+  expect(buttonElement).toBeInTheDocument();
 });
